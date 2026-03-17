f358858b1ce0672fcbd617a3db08356f7a4358ae
Giulio <giulio@DESKTOP-7BDD4BM.>
2026-03-17 15:16:25 +0100

    Ignoro la cartella build e rimuovo file tracciati

--- build/CMakeFiles/SIMPLESAD.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/SIMPLESAD"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/SIMPLESAD.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- build/CMakeFiles/SimpleSad.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for SimpleSad.
-# This may be replaced when dependencies are built.
